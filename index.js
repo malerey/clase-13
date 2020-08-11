fd17796c50759ccb8231fb030871196962e1691b
malerey <reymalena@gmail.com>
2020-08-10 22:05:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,93 @@
+// // Esto es un comentario
+
+// // Alertas:
+// alert("Esto es un alerta")
+
+// // Prompts:
+// prompt("Te gusta mas Marvel o DC?")
+
+// // Confirm: 
+// confirm("Sex and the City, es parte de DC?")
+
+// Consola
+// console.log("Un mensaje en la consola")
+
+// Variables
+
+// Las variables guardan DATOS
+
+// Los datos pueden ser
+// 1. Numeros
+// 2. "Strings", cadenas de texto
+// Operaciones basicas con strings (concatenacion) y operaciones con numeros
+
+
+
+// VARIABLES 
+
+// Hay tres maneras de declarar variables 
+// la primera es let --> permiten cambiar su valor
+// la segunda es const --> no permiten cambiar su valor 
+// la tercera es var --> no la usamos 
+
+// hay palabras reservadas --> no se puede usar como nombre de variable 
+
+// DATOS 
+
+// Strings ==> cadenas de texto, que identificamos con "comillas"
+// Numeros ==> 3, 4, 5
+// Booleano ==> true, false
+
+
+// STRINGS
+// comillas dobles ""
+// comillas simples ''
+// template strings `` --> permiten agregar saltos de linea y variables intercaladas
+
+// NUMEROS
+
+// let edad = 33
+// let anioDeNacimiento = 1987
+
+// // operadores aritmeticos
+// let suma = anioDeNacimiento + edad
+// // resta - 
+// // division / 
+// // multiplicacion *
+// // % --> resto o modulo. 
+
+// operadores de agrupamiento
+// let operacionCompleja = 2 / (3 + 6)
+
+
+
+// operadores relacionales o de comparacion 
+// let esMayor = anioDeNacimiento > edad
+// let esMenor = anioDeNacimiento < edad
+// let compararNumeros = 3 > 5
+
+// operador de asignacuion 
+// let nombre = "Malena"
+
+// operador de concatenacion 
+// nombre + apellido 
+
+
+// coercion a tipos
+// Number(dato) --> convierte "dato" a numero
+// String(dato) --> convierte "dato" a string
+// Boolean(dato) --> convierte "dato" a booleano
+
+// typeof
+// me permite saber que tipo tiene un dato 
+
+
+
+// poniendo todo en practica
+
+// let edad = prompt("Decime tu edad")
+// console.log(typeof edad) // --> me dice que edad es un string
+// let edadComoNumero = Number(edad)
+// const anioDeNacimiento = 2020 - edadComoNumero
+// alert(`Naciste en el año ${anioDeNacimiento}`)
+
